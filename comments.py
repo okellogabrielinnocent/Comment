6682ab6b3cd5a5d1b67ceda4a808bf757d5d6383
unknown <getjames20@gmail.com>
2018-07-02 09:13:07 +0300

    Edit comments

--- comments.py
+++ comments.py
@@ -2,7 +2,7 @@ class Comment:
     
     comments = []
     
-    def __init__ (self):
+    def __init__ (self, user_id, author, comment_id, c_timestamp):
         self.user_id = user_id
         self.author = author
         self.comment_id = comment_id
@@ -10,9 +10,34 @@ class Comment:
         self.c_timestamp = c_timestamp
     
     def add_comment(self):
-        new_comment = comments.append(Comment('U01', 'Ronald', 'C01', 'Hello', '02/06/18'))
+        new_comment = ' '
+        
+        while not new_comment:
+            new_comment = input('Enter your comment: ')
     
-    print comments
+        print('Comment saved')
+        comments.append(new_comment)
+
+        the_comment = {
+            'user_id': '......',
+            'author' : '......',
+            'comment_id' : 'C01',
+            'comment_body' : new_comment,
+            'c_timestamp' : '02/07/18'
+        }
+
+    def edit_comment(self):
+        for comment in the_comment:
+            if 'comment_id' = 'C01':
+                edited_comment = input('Enter new comment')
+                'comment_body' = edited_comment
+        else:
+            
+
+
+       
+        
+
 
     
 
